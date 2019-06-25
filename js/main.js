eabd6ab0b2bc7ec74587f46beb8594c27eb7a774
hinode000jp <takenoko000jp@gmail.com>
2019-06-25 12:34:19 +0900

    add scroll

--- js/main.js
+++ js/main.js
@@ -9,4 +9,44 @@ window.onload = ()=> {
         speed: 500,
         effect: 'fade',
     });
+
+    function smoothLink(headH = 0) {
+        const interval = 10;
+        const divisor = 8;
+        const range = (divisor / 2) + 1;
+        const links = document.querySelectorAll('a[href^="#"]');
+        console.log(links);
+
+
+        for (let i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function(e) {
+                console.log(i);
+
+                e.preventDefault();
+                let toY;
+                let nowY = window.pageYOffset;
+                const href = e.target.getAttribute('href');
+                const target = document.querySelector(href);
+                const targetRect = target.getBoundingClientRect();
+                const targetY = targetRect.top + nowY - headH;
+
+                (function doScroll() {
+                    toY = nowY + Math.round((targetY - nowY) / divisor);
+                    window.scrollTo(0, toY);
+                    nowY = toY;
+
+                    if (document.body.clientHeight - window.innerHeight < toY) {
+                        window.scrollTo(0, document.body.clientHeight);
+                        return;
+                    }
+                    if (toY >= targetY + range || toY <= targetY - range) {
+                        window.setTimeout(doScroll, interval);
+                    } else {
+                        window.scrollTo(0, targetY);
+                    }
+                })();
+            });
+        }
+    }
+    smoothLink();
 }
